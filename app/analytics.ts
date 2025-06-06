3d8eb94db8da488a2aa48413fa56d8e0ef9e627e
Logan Ashamallah <loganash500@gmail.com>
2025-06-05 20:17:21 -0700

    Added analytics.ts and updated layout.tsx

---
+++ app/analytics.ts
@@ -0,0 +1,13 @@
+declare global {
+  interface Window {
+    gtag: (...args: any[]) => void
+  }
+}
+
+export const GA_MEASUREMENT_ID = 'G-8R0B7DFPJ4'
+
+export const pageview = (url: string) => {
+  if (typeof window !== 'undefined' && window.gtag) {
+    window.gtag('config', GA_MEASUREMENT_ID, { page_path: url })
+  }
+}
