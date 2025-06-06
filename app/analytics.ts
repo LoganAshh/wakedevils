5c725bae8d9b43912773a429e1cba09349db8265
Logan Ashamallah <loganash500@gmail.com>
2025-06-05 20:28:08 -0700

    Fix analytics.ts

--- app/analytics.ts
+++ app/analytics.ts
@@ -1,13 +1,15 @@
 declare global {
   interface Window {
-    gtag: (...args: any[]) => void
+    gtag: (...args: [string, string, Record<string, any>?]) => void
   }
 }
 
 export const GA_MEASUREMENT_ID = 'G-8R0B7DFPJ4'
 
 export const pageview = (url: string) => {
-  if (typeof window !== 'undefined' && window.gtag) {
-    window.gtag('config', GA_MEASUREMENT_ID, { page_path: url })
+  if (typeof window !== 'undefined' && typeof window.gtag === 'function') {
+    window.gtag('config', GA_MEASUREMENT_ID, {
+      page_path: url,
+    })
   }
 }
