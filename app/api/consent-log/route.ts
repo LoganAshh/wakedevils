42709fbc1646c85da5f72c0922f553bbde1184ac
Logan Ashamallah <loganash500@gmail.com>
2025-06-12 20:19:56 -0400

    Fix google sheets connection

--- app/api/consent-log/route.ts
+++ app/api/consent-log/route.ts
@@ -3,7 +3,7 @@ export async function POST(req: Request) {
     const body = await req.json()
 
     const response = await fetch(
-      'https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLjdofCnx-kNjBP4ac7GrmOagNvQvFHm20SpQau9M7jR_enxfEtVRVW71qjho7zHnOtJlwOEC9THOHakfw2zjeNO1abG7QTQI7VgdczCtcy6jspk_C71JbhA_AX8vyEQK0cyUhKLwfxaC60SRmcbMAtk3dzCUCad1f_QYTr5nmLxeyiEf_vrCl_dOPeiyfBxBIew4F7cASZBwg-6jHKtFRNkLd0ah_PnCROhCZmL5v-AlZVPuyjO3xi4KIkt3FUPWdruOxJ1v6aO3V91_bgnqi-1GQ4U9w&lib=MdvoWplYCNWgBTQ3yjpFP6V8SLOz3GMtV',
+      'https://script.google.com/macros/s/AKfycbyHMujndHIMU69yrw1EB9ogJnWoixtjJpcDf4UjObk-ICT1fjYW39kBd4NOmzldGx7O/exec',
       {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
