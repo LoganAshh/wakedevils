7097af954ce40d50a0210ee213507270cd196a4c
Logan Ashamallah <loganash500@gmail.com>
2025-06-12 22:31:06 -0400

    Fix mobile scroll Try #2

--- app/page.tsx
+++ app/page.tsx
@@ -103,7 +103,7 @@ export default function HomePage() {
 
       <Header />
 
-      <section className="relative w-full h-[600px] md:h-[80vh] font-sans">
+      <section className="relative w-full h-[600px] md:h-[80vh] min-h-[600px] md:min-h-[80vh] max-h-[600px] md:max-h-[80vh] font-sans">
         <button
           ref={prevRef}
           className={`swiper-button-prev-custom animate-arrow-left absolute left-4 top-1/2 -translate-y-1/2 z-30 text-white text-8xl cursor-pointer transition-all duration-700 ease-out ${
@@ -159,7 +159,7 @@ export default function HomePage() {
             >
               {heroImages.map((img, i) => (
                 <SwiperSlide key={i}>
-                  <div className="relative w-full h-[600px] md:h-[80vh]">
+                  <section className="relative w-full h-[600px] md:h-[80vh] min-h-[600px] md:min-h-[80vh] max-h-[600px] md:max-h-[80vh] font-sans">
                     <Image
                       src={`/images/heros/${img.src}`}
                       alt={`Slide ${i + 1}`}
