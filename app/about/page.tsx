760071c633a50c4824169be67b6d4f752a3827b0
Logan Ashamallah <loganash500@gmail.com>
2025-07-08 16:43:45 -0700

    Add description for Nick

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -69,7 +69,7 @@ export default function AboutPage() {
       name: "Nick Hoskins",
       title: "2024 Vice President / Team Captain",
       image: "/images/board/nick.jpg",
-      bio: "Description",
+      bio: "I’m Nick. I come from Toronto, Canada and my favorite pastimes are being nice to people, speaking French, and freezing in the cold. I’m a Junior majoring in film, with a minor in interpretive dance! I play the drums but my real passion is to sing and dance!",
     },
     {
       name: "Cooper Bassi",
