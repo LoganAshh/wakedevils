778cc676ca7ef3452b9c7d8f90656c872bbb64ad
Logan Ashamallah <loganash500@gmail.com>
2025-08-04 15:04:02 -0700

    Update, crop, edit, convert, compress picture of Jalen

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -32,7 +32,7 @@ export default function AboutPage() {
     {
       name: "Jalen Artiles",
       title: "Secretary",
-      image: "/images/board/jalen.jpg",
+      image: "/images/board/jalen.jpg?v=2",
       bio: "Hi! My name is Jalen, and I’m a senior studying cybersecurity with an emphasis in business. I'm originally from California, and I love to surf, snowboard, play guitar, hit the gym, socialize, cook pesto pasta, and create new vocabulary words. I love being a Wake Devil because everyone is super inclusive, brings great energy, and I can go out on a boat whenever my heart desires.",
     },
     {
