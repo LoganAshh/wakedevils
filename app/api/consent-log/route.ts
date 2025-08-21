7492dd327a70173fd8591f5b28b57a532e2413e5
Logan Ashamallah <loganash500@gmail.com>
2025-08-21 12:37:16 -0700

    Update link

--- app/api/consent-log/route.ts
+++ app/api/consent-log/route.ts
@@ -3,7 +3,7 @@ export async function POST(req: Request) {
     const body = await req.json();
 
     const response = await fetch(
-      "https://script.google.com/macros/s/AKfycbyPaR30kk5TxIvxciruagVoImIQD--HEakImciynw32EEgjQzrK6de6jFVnYKKQnmEl/exec",
+      "https://script.google.com/macros/s/AKfycbzaLow8aA6jD7xUtUPNqr0pZT4CMzpQrC10BLV2js8IqZ0G6dy2opzv_9bycgmcq_z2/exec",
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
